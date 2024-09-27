9c3a53e72199f3c731e39b28d8cb73964d0d6f67
Amit6263 <62639186amitsharma@gmail.com>
2024-09-27 12:46:04 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,8 +1,35 @@
 const getTotalIsles = function (grid) {
+  if(grid===null || grid.length===0){
+    return 0;
+  }
+  let totalIslands = 0;
 
+  const numRows = grid.length;
+  const numCols = grid[0].length;
 
-  // write your code here
+  const dfs = (row, col)=>{
+    if(row<0 || row>=numRows || col<0 || col>=numCols || grid[row][col] === 'W'){
+      return;
+    }
 
+    grid[row][col] = 'W';
+
+    dfs(row-1, col);
+    dfs(row+1, col);
+    dfs(row, col-1);
+    dfs(row, col+1);
+  };
+
+  for(let i=0; i<numRows; i++){
+    for(let j=0; j<numCols; j++){
+      if(grid[i][j]==='L'){
+        totalIslands++;
+        dfs(i, j);
+
+      }
+    }
+  }
+  return totalIslands;
 };
 
 module.exports = getTotalIsles;
